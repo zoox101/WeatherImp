5b24d0fb4c3922fce0943d0d2075d34fa95d8642
zoox101 <william@thebookers.net>
2017-01-26 16:25:25 -0600

    Working on SOAP Request

--- src/ZZZTesting/SOAPRequest.java
+++ src/ZZZTesting/SOAPRequest.java
@@ -1,41 +1,49 @@
 package ZZZTesting;
+import java.io.IOException;
 import javax.xml.soap.*;
-
-import org.omg.CORBA.Request;
 
 public class SOAPRequest {
 	
-	public SOAPRequest(String url) throws UnsupportedOperationException, SOAPException {
+	public SOAPRequest(String url) throws UnsupportedOperationException, SOAPException, IOException {
+		
 		//Creating connection
 		SOAPConnectionFactory factory = SOAPConnectionFactory.newInstance();
 		SOAPConnection connection = factory.createConnection();
 		
+		SOAPMessage message = requestLatLon("74137");
 		
-		SOAPMessage response = connection.call(request(url), url);
-		System.out.println(response);
+		System.out.println(message.getSOAPBody().getFirstChild().getFirstChild().getTextContent());	
+		
+		SOAPMessage response = connection.call(message, url);
+		message.writeTo(System.out); System.out.println(""); response.writeTo(System.out); System.out.println("");
+		
+		String textresponse = response.getSOAPBody().getFirstChild().getFirstChild().getTextContent();
+		System.out.println(textresponse);
+		
+			
+		System.out.println("Ending");
 		connection.close();
 	}
 	
-	public static void main(String[] args) throws SOAPException {
-		System.out.println(request("testing"));
+	public static void main(String[] args) throws SOAPException, IOException {
+		new SOAPRequest("http://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php");
 	}
 	
-	public static SOAPMessage request(String server) throws SOAPException {
-		
+	public static SOAPMessage requestLatLon(String zipcode) throws SOAPException {
+		 
 		//Creating objects
 		MessageFactory factory = MessageFactory.newInstance();
 		SOAPMessage message = factory.createMessage();
-		SOAPPart part = message.getSOAPPart();
-		SOAPEnvelope envelope = part.getEnvelope();
-		SOAPBody body = envelope.getBody();
+		SOAPBody body = message.getSOAPBody();
 		
-		//Specifying request
-		envelope.addNamespaceDeclaration("TestingNameSpaceDeclaration", "http://ws.cdyne.com/");
-		body.addChildElement("LatLonListZipCode", "TestingNameSpaceDeclaration");
+		//Editing message body
+		message.getSOAPHeader().detachNode();
+		SOAPElement llzip = body.addChildElement("LatLonListZipCode");
+		SOAPElement ziplist = llzip.addChildElement("listZipCodeList").addTextNode(zipcode);
+		ziplist.setAttribute("type", "xsd:string");
 		
+		//Returning message
 		return message;
-		
-		
 	}
 
 }
